583aae8bc0317b21a374a1374919950c91de2ba0
Jen Channer <channer.jenny03@gmail.com>
2020-11-25 12:19:22 +0000

    yes

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import matplotlib.pyplot as plt
 from geopy import distance
 import numpy as np
 import pandas as pd
-import plotly.plotly as py
-import plotly.graph_objs as go
+import chart_studio.plotly as py
 import haversine
 
 gpx_file = open('gpxfiles/Bournemouth_Half.gpx', 'r')
@@ -18,8 +17,40 @@ track = gpx.tracks[0]
 segment = track.segments[0]
 points = segment.points
 
-data = []
-for p in points:
-    data.append((p.longitude, p.latitude, p.elevation, p.time))
 
-print(data)
+#for p in points:
+ #   data.append((p.longitude, p.latitude, p.elevation, p.time))
+
+df = pd.DataFrame(columns=['long', 'lat', 'elev', 'time'])
+
+for point in points:
+    df = df.append({'long': point.longitude, 'lat': point.latitude, 'elev': point.elevation, 'time': point.time}, ignore_index=True)
+
+#print (df)
+
+#plotting data
+
+plot= plt.plot(df['long'], df['lat'])
+print (dir(plot))
+
+
+
+
+#distance formula: distance_3d = sqrt(distance_2d**2 + (elev2-elev1)**2)
+
+elevDif = [0]
+timeDif = [0]
+distVin = [0]
+distHav = [0]
+distVinNoElev = [0]
+distHavNoAlt = [0]
+distDifHav2D = [0]
+distDifVin2D = [0]
+
+for index in range(len(points)):
+    if index == 0:
+        pass
+    else:
+        start = points[index-1]
+        stop = points[index]
+        distanceVin2D = 
