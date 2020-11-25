b6e9fa544a9f0c3021681baf111339289ea0f2f5
Jen Channer <channer.jenny03@gmail.com>
2020-11-25 11:32:03 +0000

    Initial parse of gpx file

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-x = [1, 2, 3, 4]
-for a in x:
-    print(str(a))
+import gpxpy
+import gpxpy.gpx
+import datetime
+import matplotlib.pyplot as plt
+from geopy import distance
+import numpy as np
+import pandas as pd
+import plotly.plotly as py
+import plotly.graph_objs as go
+import haversine
 
-print("Hello world!!")
+gpx_file = open('gpxfiles/Bournemouth_Half.gpx', 'r')
+
+gpx = gpxpy.parse(gpx_file)
+print (len(gpx.tracks))
+
+track = gpx.tracks[0]
+segment = track.segments[0]
+points = segment.points
+
+data = []
+for p in points:
+    data.append((p.longitude, p.latitude, p.elevation, p.time))
+
+print(data)
