ad91ca1d787fa2fde65b634487d2af75a6018e15
Jen Channer <channer.jenny03@gmail.com>
2020-11-27 12:02:06 +0000

    yes

--- main.py
+++ main.py
@@ -17,16 +17,13 @@ segment = track.segments[0]
 points = segment.points
 
 # for ex in p.extensions[0]: print('ex' + ex.text)
+data = []
 
-df = pd.DataFrame(columns=['long', 'lat', 'elev', 'time'])
 
 for point in points:
-    df = df.append({'long': point.longitude, 'lat': point.latitude, 'elev': point.elevation, 'time': point.time}, ignore_index=True)
+    data.append({'long': point.longitude, 'lat': point.latitude, 'elev': point.elevation, 'time': point.time})
 
-#print (df)
-
-#plotting data
-
+df = pd.DataFrame(data=data, columns=['long', 'lat', 'elev', 'time'])
 
 #distance formula: distance_3d = sqrt(distance_2d**2 + (elev2-elev1)**2)
 
@@ -36,6 +33,7 @@ distHav = [0]
 distHavNoElev = [0]
 distDifHav2D = [0]
 #distanceHav2D = [0]
+pace = [0]
 
 for index in range(1, len(points)):
     start = points[index-1]
@@ -51,19 +49,23 @@ for index in range(1, len(points)):
     timeDelta = (stop.time - start.time).total_seconds()
     timeDif.append(timeDelta)
     distHav.append(distHav[-1] + distanceHav3D)
+    pace.append(16.66667/ (distanceHav3D / timeDelta))
+
 
 df['distHave2D']= distHavNoElev
 df['distHav3D'] = distHav
 df['elevDif'] = elevDif
 df['timeDif'] = timeDif
+df['pace'] = pace
 
 print('Haversine 2D : ', distHavNoElev[-1], 'm')
 print('Haversine 3D : ', distHav[-1], 'm')
 print('Total time : ', floor(sum(timeDif)/60), 'min', int(sum(timeDif)%60), 'sec')
+#print('Pace : ', 16.666667 / pace, 'min/km')
 
-gain = sum([x for x in df['elevDif']])
+gain = sum([x for x in df['elevDif'] if x>0])
 print('Gain = {0}'.format(gain))
-print(df)
+#print(df)
 plt.plot(df['long'], df['lat'])
 plt.title('Plot', fontsize=14)
 plt.xlabel('long', fontsize=14)
@@ -71,6 +73,7 @@ plt.ylabel('lat', fontsize=14)
 plt.grid(False)
 
 plt.savefig('Plot.png')
+#plt.show()
 
 plt.plot(df['distHav3D'], df['elev'])
 plt.title('Elevation', fontsize=14)
@@ -79,4 +82,10 @@ plt.ylabel('Elevation', fontsize=14)
 plt.grid(False)
 
 plt.savefig('Elevation.png')
-plt.show()
+#plt.show()
+
+averagePace = sum(df['pace']/ len (df['pace']))
+averagePaceMins = floor(averagePace)
+averagePaceSecs = int(60 * (averagePace - averagePaceMins))
+
+print('average pace : {0}:{1}min/km'.format(averagePaceMins, averagePaceSecs))
